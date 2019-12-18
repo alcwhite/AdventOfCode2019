12893776f89a919cf38f7d24678658dc05927e97
Al White <alcwhite13@gmail.com>
2019-12-18 14:34:40 -0500

    Some tests fail

--- Day3/Wires.cs
+++ Day3/Wires.cs
@@ -6,9 +6,101 @@ namespace wire_sorting
 {
     public static class Wires
     {
-        public static int ManhattanDistance(string[] input)
+        public static List<(int x, int y, int steps)> Locations(List<string> wire)
         {
-            return 0;
+            var locations = new List<(int x, int y, int steps)>();
+            var start = (x: 0, y: 0, steps: 0);
+            foreach (string path in wire)
+            {
+                int i = 0;
+                var direction = path.Substring(0, 1);
+                var number = int.Parse(path.Substring(1));
+                if (direction == "U")
+                {    i = 0;
+                    while (i < number)
+                    {
+                        start.y++;
+                        start.steps++;
+                        locations.Add(start);
+                        i++;
+                    }
+                }
+                if (direction == "D")
+                {
+                    i = 0;
+                    while (i < number)
+                    {
+                        start.y--;
+                        start.steps++;
+                        locations.Add(start);
+                        i++;
+                    }
+                }
+                if (direction == "R")
+                {
+                    i = 0;
+                    while (i < number)
+                    {
+                        start.x++;
+                        start.steps++;
+                        locations.Add(start);
+                        i++;
+                    }
+                }
+                if (direction == "L")
+                {
+                    i = 0;
+                    while (i < number)
+                    {
+                        start.x--;
+                        start.steps++;
+                        locations.Add(start);
+                        i++;
+                    } 
+                }  
+            }    
+            return locations;   
         }
+        public static (List<(int x, int y, int steps)> locations1, List<(int x, int y, int steps)> locations2) AllLocations(List<string> listInput)
+        {
+            var wire1 = listInput[0].Split(',').ToList();
+            var wire2 = listInput[1].Split(',').ToList();
+            var locations1 = Locations(wire1);
+            var locations2 = Locations(wire2);
+            return (locations1: locations1, locations2: locations2);
+        }
+        public static List<(int x, int y)> Intersections(List<string> input)
+        {
+            var allLocations = AllLocations(input);
+            var intersectingLocations = allLocations.locations1.Where(stop => allLocations.locations2.Exists(otherStop => stop.x == otherStop.x && stop.y == otherStop.y)).ToList();
+            return intersectingLocations.Select(x => (x: x.x, y: x.y)).ToList();
+        }
+        public static List<int> ManhattanDistance(List<string> input)
+        {
+            var intersections = Intersections(input);
+            return intersections.Select(x => Math.Abs(x.x) + Math.Abs(x.y)).ToList();
+        }
+        public static int MinDistance(List<string> input)
+        {
+            var distances = ManhattanDistance(input);
+            return distances.Min();
+        }
+        public static int Steps(List<string> input)
+        {
+            var minDistance = MinDistance(input);
+            var allDistances = ManhattanDistance(input);
+            var allLocations = AllLocations(input);
+            var intersections = Intersections(input);
+            var minIndex = allDistances.FindIndex(x => x == minDistance);
+            var location = intersections[minIndex];
+            var allWire1Locations = allLocations.locations1;
+            var allWire2Locations = allLocations.locations2;
+            var wire1Location = allWire1Locations.FindIndex(stop => stop.x == location.x && stop.y == location.y);
+            var wire2Location = allWire2Locations.FindIndex(stop => stop.x == location.x && stop.y == location.y);
+            var wire1Stop = allWire1Locations[wire1Location];
+            var wire2Stop = allWire2Locations[wire2Location];
+            return wire1Stop.steps + wire2Stop.steps;
+        }
+
     }
 }
--- Day3/WiresTest.cs
+++ Day3/WiresTest.cs
@@ -1,29 +1,57 @@
 using Xunit;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
-public class FuelTest
+public class WireTest
 {
-    string[] testInput1 = {"R8,U5,L5,D3", "U7,R6,D4,L4"};
-    string[] testInput2 = {"R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"};
-    string[] testInput3 = {"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"};
-    string[] input = File.ReadAllLines("../../../input.txt");
+    static string[] testInput1Arr = {"R8,U5,L5,D3", "U7,R6,D4,L4"};
+    List<string> testInput1 = testInput1Arr.ToList();
+    static string[] testInput2Arr = {"R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"};
+    List<string> testInput2 = testInput2Arr.ToList();
+    static string[] testInput3Arr = {"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"};
+    List<string> testInput3 = testInput3Arr.ToList();
+    static string[] inputArr = File.ReadAllLines("../../../input.txt");
+    List<string> input = inputArr.ToList();
     [Fact]
     public void Part1_Puzzle() 
     {
-        Assert.Equal(12, wire_sorting.Wires.ManhattanDistance(input));
+        Assert.Equal(3247, wire_sorting.Wires.MinDistance(input));
     }
     [Fact]
     public void Part1_Test1() 
     {
-        Assert.Equal(6, wire_sorting.Wires.ManhattanDistance(testInput1));
+        Assert.Equal(6, wire_sorting.Wires.MinDistance(testInput1));
     }
+    [Fact]
     public void Part1_Test2() 
     {
-        Assert.Equal(159, wire_sorting.Wires.ManhattanDistance(testInput2));
+        Assert.Equal(159, wire_sorting.Wires.MinDistance(testInput2));
     }
+    [Fact]
     public void Part1_Test3() 
     {
-        Assert.Equal(135, wire_sorting.Wires.ManhattanDistance(testInput3));
+        Assert.Equal(135, wire_sorting.Wires.MinDistance(testInput3));
+    }
+
+    [Fact]
+    public void Part2_Puzzle() 
+    {
+        Assert.Equal(3247, wire_sorting.Wires.Steps(input));
+    }
+    [Fact]
+    public void Part2_Test1() 
+    {
+        Assert.Equal(40, wire_sorting.Wires.Steps(testInput1));
+    }
+    [Fact]
+    public void Part2_Test2() 
+    {
+        Assert.Equal(610, wire_sorting.Wires.Steps(testInput2));
+    }
+    [Fact]
+    public void Part2_Test3() 
+    {
+        Assert.Equal(410, wire_sorting.Wires.Steps(testInput3));
     }
 }
