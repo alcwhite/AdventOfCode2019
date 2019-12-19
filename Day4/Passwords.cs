b13c7f0fe1f14f012f12dc58949181d7ba7881fb
Al White <alcwhite13@gmail.com>
2019-12-19 15:41:27 -0500

    Day4 part 2 doesn't work

---
+++ Day4/Passwords.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace password_puzzle
+{
+    public static class Passwords
+    {
+        public static bool PasswordPasses(int password)
+        {
+            var doubleNums = false;
+            var decrease = false;
+            var passwordString = password.ToString();
+            var passwordList = passwordString.ToList();
+            int count = 0;
+            foreach (char digit in passwordList)
+            {
+                int digitInt = int.Parse(digit.ToString());
+                if (count != 0)
+                {
+                    string formerDigit = passwordList[count - 1].ToString();
+                    var formerDigitInt = int.Parse(formerDigit);
+                    if (digitInt < formerDigitInt && !decrease)
+                        decrease = true;
+                    if (digit == passwordList[count - 1] && !doubleNums)
+                        doubleNums = true;
+                }
+                count++;
+            }
+
+            return !decrease && doubleNums;
+        }
+        public static int PasswordCount((int lowerBound, int upperBound) input)
+        {
+            int count = 0;
+            for (int number = input.lowerBound; number <= input.upperBound; number++)
+            {
+                var doesItPass = PasswordPasses(number);
+                if (doesItPass) count++;
+            }
+            return count;
+        }
+        public static bool PasswordPassesForReal(int password)
+        {
+            var passesFirstSet = PasswordPasses(password);
+            bool largerSet = false;
+            bool extraDouble = false;
+            if (passesFirstSet)
+            {
+                var passwordString = password.ToString();
+                var passwordList = passwordString.ToList();
+                int count = 0;
+                foreach (char digit in passwordList)
+            {
+                int digitInt = int.Parse(digit.ToString());
+                if (count > 1)
+                {
+                    string formerDigit = passwordList[count - 2].ToString();
+                    var formerDigitInt = int.Parse(formerDigit);
+                    string nextDigit = passwordList[count + 1].ToString();
+                    var nextDigitInt = int.Parse(nextDigit);
+                    if (digitInt == int.Parse(passwordList[count - 1].ToString()))
+                    {
+                        if (digitInt == formerDigitInt && digitInt == nextDigitInt)
+                            largerSet = true;
+                        if (!largerSet)
+                            extraDouble = true;
+                    }
+                    
+                }
+                if (count == 1)
+                {
+                    string nextDigit = passwordList[count + 1].ToString();
+                    var nextDigitInt = int.Parse(nextDigit);
+                    if (digitInt == int.Parse(passwordList[count - 1].ToString()))
+                    {
+                        
+                        if (digitInt != nextDigitInt)
+                            extraDouble = true;
+                    }
+                    
+                }
+                count++;
+            }
+            }
+
+            return passesFirstSet && (extraDouble || !largerSet);
+        }
+        public static int PasswordCountForReal((int lowerBound, int upperBound) input)
+        {
+            int count = 0;
+            for (int number = input.lowerBound; number <= input.upperBound; number++)
+            {
+                var doesItPass = PasswordPassesForReal(number);
+                if (doesItPass) count++;
+            }
+            return count;
+        }
+
+    }
+}
---
+++ Day4/PasswordsTest.cs
@@ -0,0 +1,55 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PasswordsTest
+{
+    static string inputString = ("387638-919123");
+    static List<string> inputList = inputString.Split('-').ToList();
+    (int lowerBound, int upperBound) input = (lowerBound: int.Parse(inputList[0]), upperBound: int.Parse(inputList[1]));
+    [Fact]
+    public void Part1_Puzzle() 
+    {
+        Assert.Equal(466, password_puzzle.Passwords.PasswordCount(input));
+    }
+    [Fact]
+    public void Part1_Test1() 
+    {
+        Assert.True(password_puzzle.Passwords.PasswordPasses(111111));
+    }
+    [Fact]
+    public void Part1_Test2() 
+    {
+        Assert.False(password_puzzle.Passwords.PasswordPasses(223450));
+    }
+    [Fact]
+    public void Part1_Test3() 
+    {
+        Assert.False(password_puzzle.Passwords.PasswordPasses(123789));
+    }
+    [Fact]
+    public void Part2_Puzzle() 
+    {
+        Assert.Equal(337, password_puzzle.Passwords.PasswordCountForReal(input));
+    }
+    [Fact]
+    public void Part2_Test1() 
+    {
+        Assert.True(password_puzzle.Passwords.PasswordPassesForReal(112233));
+    }
+    [Fact]
+    public void Part2_Test2() 
+    {
+        Assert.False(password_puzzle.Passwords.PasswordPassesForReal(123444));
+    }
+    [Fact]
+    public void Part2_Test3() 
+    {
+        Assert.True(password_puzzle.Passwords.PasswordPassesForReal(111122));
+    }
+    [Fact]
+    public void Part2_Test4() 
+    {
+        Assert.True(password_puzzle.Passwords.PasswordPassesForReal(221111));
+    }
+}
